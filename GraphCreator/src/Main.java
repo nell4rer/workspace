7effe888e09a0483817c60c7dc477ef4efddf3d0
un1acker <un1acker@yandex.ru>
2014-10-15 18:19:51 +0400

    add project

---
+++ GraphCreator/src/Main.java
@@ -0,0 +1,13 @@
+
+import view.GraphMain;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame myGraph = new GraphMain();
+        myGraph.setVisible(true);
+
+    }
+
+}
